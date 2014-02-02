418d51c9ea7f261dba4786037043143f21d4031a
David Hay <misterhay@gmail.com>
2014-02-02 09:08:25 -0700

    changing the ser.write

--- OSC_recieve_serial_send.py
+++ OSC_recieve_serial_send.py
@@ -12,9 +12,11 @@ def yamahaHandler(address, typetag, value, source): #a function for the Yamaha r
     #typetag should be i
     #value should be the number of the command we want to send
     #source should be the IP address and port it came from
-    print str(value) #find some way to convert this to fit in the ser.write command
-    ser = serial.Serial(7, 9600) #open COM8 at 9600 baud
-    ser.write('1/n') #toggle power
+    message = str(value) #convert the value to a string 
+    ser = serial.Serial(7, 9600) #open COM8 at 9600 baud and 8N1
+    ser.write(value) #send the command that we received
+    #ser.write('1') #toggle power
+    ser.write('/n') #new line character to make it go
     ser.close()
 
 server.addMsgHandler("/yamaha", yamahaHandler) #the OSC address
