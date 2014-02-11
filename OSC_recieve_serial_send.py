104c475535a053d6283e632b0a1245c799fd8ee1
David Hay <misterhay@gmail.com>
2014-02-10 19:10:41 -0700

    everything is working
    Requires TouchOSC for iOS or Android.

--- OSC_recieve_serial_send.py
+++ OSC_recieve_serial_send.py
@@ -3,7 +3,7 @@ import OSC #requires pyOSC
 import time, threading
 
 
-receiveAddress = ('127.0.0.1', 9000)
+receiveAddress = ('0.0.0.0', 9000)
 server = OSC.OSCServer(receiveAddress) #set up the OSC server
 server.addDefaultHandlers() #not really necessary
 
@@ -13,6 +13,7 @@ def yamahaHandler(address, typetag, value, source): #a function for the Yamaha r
     #value should be the number of the command we want to send
     #source should be the IP address and port it came from
     message = str(value)[1] #convert the value to a string, but only x in [x]
+    print value
     ser = serial.Serial(7, 9600) #open COM8 at 9600 baud and 8N1
     ser.write(message) #send the command that we received
     ser.write('/n') #new line character to make it go
@@ -20,7 +21,7 @@ def yamahaHandler(address, typetag, value, source): #a function for the Yamaha r
 
 server.addMsgHandler("/yamaha", yamahaHandler) #the OSC address
 
-print "use ctrl-c to quit"
+print "When you're finished, use ctrl-c to quit"
 threadingServer = threading.Thread(target = server.serve_forever)
 threadingServer.start() #start the server thread we just defined
 
